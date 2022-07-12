7eed9f6750c5190042073d3eee2489331666ba58
聂升 <ns@xkool.org>
2022-07-12 16:49:17 +0800

    init template

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+// // This starter template is using Vue 3 <script setup> SFCs
+// // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="user">
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action="">
+        <div class="layui-form-item">
+          <label class="layui-form-label">手机/邮箱</label>
+          <div class="layui-input-block">
+            <input type="text" name="title" required lay-verify="required" placeholder="请输入手机/邮箱" autocomplete="off"
+              class="layui-input">
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input type="password" name="title" required lay-verify="required" placeholder="请输入密码" autocomplete="off"
+              class="layui-input">
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-block">
+            <input type="text" name="title" required lay-verify="required" placeholder="请输入验证码" autocomplete="off"
+              class="layui-input">
+          </div>
+        </div>
+        <button class="layui-btn" lay-submit lay-filter="formDemo">立即提交</button>
+        <a type="reset" class="forgot-pwd">忘记密码?</a>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.user {
+  background-color: #f2f2f2;
+
+  .layui-container {
+    background-color: #fff;
+  }
+
+  input {
+    width: 190px;
+  }
+
+  .forgot-pwd {
+    margin-left: 10px;
+    cursor: pointer;
+
+    &:hover {
+      color: #009688;
+    }
+  }
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  css: {
+    // css预处理器
+    preprocessorOptions: {
+      scss: {
+        // 引入 var.scss 这样就可以在全局中使用 var.scss中预定义的变量了
+        additionalData: '@import "./src/assets/scss/var.scss";',
+      },
+    },
+  },
+});
