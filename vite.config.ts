79f116d2c07c0e07e16489166916666f7793f9cf
nlovesye <894765963@qq.com>
2022-09-15 10:31:39 +0800

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/imooc-dqd-frontend/',
+  base: process.env.NODE_ENV === "production" ? "/imooc-dqd-frontend/" : "/",
   plugins: [vue()],
   css: {
     // css预处理器
@@ -12,6 +12,14 @@ export default defineConfig({
       scss: {
         // 引入 var.scss 这样就可以在全局中使用 var.scss中预定义的变量了
         additionalData: '@import "./src/assets/scss/var.scss";',
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "404.html"),
       },
     },
   },
