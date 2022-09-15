5868390163627dbec9488bda22169a71722c2779
nlovesye <894765963@qq.com>
2022-09-15 13:15:16 +0800

    feat: update rsync deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/" : "/",
+  // base: process.env.NODE_ENV === "production" ? "/" : "/",
   plugins: [vue()],
   css: {
     // css预处理器
